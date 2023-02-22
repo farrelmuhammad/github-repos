092d8a1959bde9c46939a0ecd345b1b0367cb5eb
Farrel Muhammad <farrelf11@gmail.com>
2023-02-22 20:33:12 +0700

    Add footer in homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -42,6 +42,9 @@ const Homepage = () => {
                     </div>
                 </div>
             </div>
+            <footer className="bg-gray-100 text-center text-xs p-3 absolute bottom-0 w-full border-t border-gray-400">
+                &copy; 2023 Farrel Muhammad.
+            </footer>
         </>
     )
 }
